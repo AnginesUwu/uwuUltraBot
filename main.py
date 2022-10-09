44740b03f84fe0a174e40c031251bf7b812c4e5b
AnginesUwu <angelmanrique46@gmail.com>
2022-10-09 16:29:56 -0500

    Uwu bot doxin functions and registers txt

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 #Hola git pull command :)
 bot_token = "5756497503:AAGupePh024UdwwUu3kePQDJ7ziDY5jWfdY"
 
-from html import entities
 from telegram import Update
 from telegram.ext import Updater, ContextTypes #SEND MESSAGES
 import logging #LOGEO
-from telegram.ext import CommandHandler #COMANDOS
+from telegram.ext import CommandHandler, MessageHandler, Filters #COMANDOS
 
 from datetime import datetime
 
@@ -16,28 +15,44 @@ dispatcher = updater.dispatcher
 #Loggin Start
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-#CODE
+#COMANDOS
 def start(update: Update, context: ContextTypes):
-    timeHour = datetime.now().strftime('%H:%M')
-
-    print(update.effective_chat.id) #print the ID of the User
-    text2 = "hola " + str(update.effective_user.first_name) + "\n Son las " + str(timeHour)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text2) # BOT'S RESPOND
-
-def personas(update: Update, context: ContextTypes):
-    print(update.effective_chat.id) #ESCRIBE EL CHAT ID DE LA PERSONA
+    print(f"\nId nueva: {update.effective_chat.id}") #ESCRIBE EL CHAT ID DE LA PERSONA
+    print(f"\nUsername: {update.effective_user.username}")
     #ALMACENA EL CHAT ID A EL ARCHIVO: personas/personas.txt
     archivo = open('personas/personas.txt', 'a')
     archivo.write(str(update.effective_chat.id)+"\n")
     archivo.close()
+
+    archivo = open('personas/usernames.txt', 'a')
+    archivo.write(str(update.effective_user.username)+"\n")
+    archivo.close()
     
-    context.bot.send_message(chat_id=update.effective_chat.id, text="AHORA ESTÁS EN LA LISTA!!") #RESPONDE A EL CHAT EN EL QUE LE HABLAN
+#    timeHour = datetime.now().strftime('%H:%M')
+
+    text2 = f"Hola! {update.effective_user.first_name} :)\n _Cuentame lo que quieras_"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text2, parse_mode="Markdown") # BOT'S RESPOND
+
+#FUNCIONES
+
+def echo(update:Update, context:ContextTypes):
+    update.message.reply_text(update.message.text, entities=update.message.entities)
+
+
+def newMessage(update:Update, context:ContextTypes):
+    print(f"\n{update.effective_user.username}: {update.message.text} | {update.message.date}\nEnviar Mensaje: ", end='')
+    
+    archivo = open(f"registros/{update.effective_user.username}.txt", 'a')
+    archivo.write(str(f"{update.message.date}: {update.message.text}\n"))
+    archivo.close()
     
 
+#eventos de comandos
 start = CommandHandler('start', start) #LLAMA A LA FUNCION start CON EL COMANDO /start
-personas = CommandHandler('personas', personas) #LLAMA A LA FUNCION personas CON EL COMANDO /personas
 dispatcher.add_handler(start) #ANADE EL COMANDO PARA QUE FUNCIONE
-dispatcher.add_handler(personas) #ANADE EL COMANDO PARA QUE FUNCIONE
+#eventes de mensajes
+newMessag = MessageHandler(Filters.text, newMessage)
+dispatcher.add_handler(newMessag)
 
 updater.start_polling() #ENCIENDE EL BOT, PARA PARAR updater.stop()
 
@@ -54,7 +69,6 @@ def sendMessage(h):
     # Markdown V1 Sintax
     #   bold: *text*    -   italic: _text_  -   link: [text](url)  -   code: `text` 
     
-    entiti = MessageEntity(type="text_link", offset=0,length=9, url="https://instagram.com/marurunk")
     personasLista = [] #CREA LISTA
     with open('personas/personas.txt', 'r') as archivo: #OBTIENE CHAT IDs OBTENIDOS GRACIAS A /personas
         for personaLinea in archivo:
@@ -63,8 +77,11 @@ def sendMessage(h):
     for i in personasLista: #MANDA UN MENSAJE A CADA UNO DE LAS subscripciones
         bot = Bot(bot_token) #DEFINE "bot" A NUESTRO TOKEN
         
-        bot.send_message(chat_id=i,text=f"{h}", parse_mode="Markdown") #MANDA MENSAJE
+        bot.send_message(chat_id=i,text=h, parse_mode="Markdown") #MANDA MENSAJE
 #        bot.send_message(chat_id=i,text=f"{h}", entities=[entiti]) #MANDA MENSAJE
+        
+
+
 while True:
     h = input("Enviar mensaje: ")
     sendMessage(h)
